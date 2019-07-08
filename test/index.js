e9a778effa3e66e2a8820f645d4d45e9f65019ae
dapize <danielpz@outlook.com>
2019-07-08 04:48:00 -0500

    fix(Core): loaded promise
    Issue #13

--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
 var waltographUIFont = new FontFace2('Waltograph UI', "url('./fonts/WaltographUI-Bold.woff2') format('woff2'), url('./fonts/WaltographUI-Bold.woff') format('woff'), url('./fonts/WaltographUI-Bold.ttf') format('truetype')", { style: 'normal', weight: 'normal' });
 
+waltographUIFont.loaded.then(function (fontObj) {
+   console.log('then de loaded');
+});
+
 waltographUIFont.load()
    .then(function (fontObj) {
+      console.log('then de load');
       document.fonts2.add(fontObj);
    })
    .catch(function (err) {
+      console.log('catch de load');
       console.log(err);
    });
+
 
