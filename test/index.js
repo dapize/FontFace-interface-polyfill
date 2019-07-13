e46f4e34c307896e5302c7dc4866f1b0d8c440d0
dapize <danielpz@outlook.com>
2019-07-12 22:48:43 -0500

    feat(Core): Events onloading, onloadingdone, onloadingerror
    Closes #05

--- test/index.js
+++ test/index.js
@@ -1,30 +1,30 @@
-var waltographUIFont = new FontFace2('Waltograph UI', "url('./fonts/WaltographUI-Bold.woff2') format('woff2'), url('./fonts/WaltographUI-Bold.woff') format('woff'), url('./fonts/WaltographUI-Bold.ttf') format('truetype')", { style: 'normal', weight: 'normal' });
+var waltographUIFont = new FontFace('Waltograph UI', "url('./fonts/WaltographUI-Bold.woff2') format('woff2'), url('./fonts/WaltographUI-Bold.woff') format('woff'), url('./fonts/WaltographUI-Bold.ttf') format('truetype')", { style: 'normal', weight: 'normal' });
 waltographUIFont.load()
    .then(function (fontObj) {
-      document.fonts2.add(fontObj);
+      document.fonts.add(fontObj);
    })
    .catch(function (err) {
       console.log(err);
    });
 
 
-var ArtBrewery = new FontFace2('Art Brewery', "url('./fonts/ArtBrewery.woff2') format('woff2'), url('./fonts/ArtBrewery.woff') format('woff')", { style: 'normal', weight: '400' });
+var ArtBrewery = new FontFace('Art Brewery', "url('./fonts/ArtBrewery.woff2') format('woff2'), url('./fonts/ArtBrewery.woff') format('woff')", { style: 'normal', weight: '400' });
 ArtBrewery.load()
    .then(function (fontObj) {
-      document.fonts2.add(fontObj);
+      document.fonts.add(fontObj);
    })
    .catch(function (err) {
       console.log(err);
    });
 
-document.fonts2.onloading = function (ffset) {
+document.fonts.onloading = function (ffset) {
    console.log('Cargando... ' + ffset.family);
 };
 
-document.fonts2.onloadingdone = function (ffset) {
+document.fonts.onloadingdone = function (ffset) {
    console.log('¡Cargada! ' + ffset.family);
 };
 
-document.fonts2.onloadingerror = function (ffset) {
+document.fonts.onloadingerror = function (ffset) {
    console.log('Error al cargar ' + ffset.family);
 };
